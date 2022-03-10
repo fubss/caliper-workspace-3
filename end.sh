984ae1af6fe8e90f5decd355ef772ace57b00e73
Ivan L <ivanlaish@yandex.ru>
2022-03-10 10:57:04 +0000

    initial commit with benchmark/get-asset 64KB x5 repeating

---
+++ end.sh
@@ -0,0 +1,20 @@
+# parse a statedb subcommand if used
+if [[ $# -ge 1 ]] ; then
+  key="$1"
+  if [[ "$key" == "leveldb" ]]; then
+      export STATEDBNAME=leveldb
+      export FABRIC_VERSION=2.4.0-goleveldb
+      shift
+  fi
+  if [[ "$key" == "rocksdb" ]]; then
+      export STATEDBNAME="rocksdb"
+      export FABRIC_VERSION="!!!!!!!!!!2.4.0-grocksdb"
+
+      shift
+  fi
+fi
+
+set -x
+
+#../fabric-samples/test-network/network.sh down
+(cd ../fabric-samples/test-network && exec ./network.sh down)
---
+++ start.sh
@@ -0,0 +1,27 @@
+## Parse mode
+if [[ $# -lt 1 ]] ; then
+  echo "Usage (lowcase): ./start.sh statedbname"
+  exit 0
+fi
+
+# parse a statedb subcommand if used
+if [[ $# -ge 1 ]] ; then
+  key="$1"
+  if [[ "$key" == "leveldb" ]]; then
+      export STATEDBNAME=leveldb
+      shift
+  fi
+  if [[ "$key" == "rocksdb" ]]; then
+      export STATEDBNAME=rocksdb
+      shift
+  fi
+fi
+
+set -x
+
+#../fabric-samples/test-network/network.sh up createChannel
+(cd ../fabric-samples/test-network && exec ./network.sh up createChannel)
+
+#-ccp path is relative to directory fabric-samples/test-network
+#(cd ../fabric-samples/test-network && exec ./network.sh deployCC -ccn fixed-asset -ccp ../../caliper-workspace-2/smart-contract/node -ccl javascript)
+(cd ../fabric-samples/test-network && exec ./network.sh deployCC -ccn fixed-asset -ccp ../../caliper-workspace-3/smart-contract/go -ccl go)
